743e164822060284c1400dd415630ef7871a4c69
moffmofu <mofumori@icloud.com>
2019-09-02 16:46:26 +0900

    after making submodule

--- getLoc.m
+++ getLoc.m
@@ -1,4 +1,4 @@
-function out=getLoc(filename,varargin)
+function copy=getLoc(filename,varargin)
 % import & translate the senseor layouts from the file exported by GPS system
 % param.1 : filename to read
 % param.2 : mode("xy"=2dim_xyPlane , "3"=3dim)
@@ -17,13 +17,18 @@ end
 
 %% readfile
 if contains(filename,'.xml')
-    % .xml
+    % .xml(export for MFF)
     data=readxml(filename);
-elseif contains(filename,'.')
-    % 
+    copy=zeros(3,129);
+    for cnt=1:129
+        out(1,cnt)=data.Children(2).Children(4).Children(cnt).;
+        copy(2,cnt)=data(cnt).y;
+    end
+elseif contains(filename,'.sfp')
+    % .sfp(export for BESA)
     data=readllllllllll();
-elseif contains(filename,'.txt')
-    %
+elseif contains(filename,'.nsi')
+    % .nsi(export for NSI)
     data=readrrrrrrrrrrr();
 else
     % invalid extension
@@ -33,20 +38,22 @@ else
 end
 
 %% reform data
+
+
 if strcmpi(mode,"xy")
-    out=zeros(2,129);
-    for cnt=1:129
-        out(:,cnt)=[data(cnt).x,data(cnt).y];
-    end
-    out=out-1; %adjust location
+    out=cutXYPlane(copy);
+    
 elseif strcmpi(mode,"3")
-    out=zeros(3,129);
-    for cnt=1:129
-        out(:,cnt)=[data(cnt).x,data(cnt).y];
-    end
-    out=out-1; %adjust location
+    % Žg‚¢•û‚ðŒ©‚ÄŒˆ‚ß‚é
 end
 
 
 
+
+end
+
+function cutXYPlane()
+    %create base plane from nasion, LPA, RPA
+    %set Cz location as normal vecter
+    %shadow each point by normal vecter
 end
