7765515f1dd5a4dbc99763b9844c3cb30b2f685d
moffmofu <mofumori@icloud.com>
2019-09-18 11:08:54 +0900

    あのてーしょん

--- tester.m
+++ tester.m
@@ -17,6 +17,9 @@ vars=sqrt(vars)/2;
 figure(1);
 scatter3(aves(1,:),aves(2,:),aves(3,:));
 title("aver.")
+xlabel("x (+:right)");
+ylabel("y (+:rostal)");
+zlabel("z (+:dorsal)");
 
 figure(2);
 scatter3(loc(1,:,1),loc(2,:,1),loc(3,:,1));
@@ -24,6 +27,12 @@ hold on
 scatter3(loc(1,:,2),loc(2,:,2),loc(3,:,2));
 %scatter3(tester(1,129:132),tester(2,129:132),tester(3,129:132))
 title("indiv.")
+xlabel("x (+:right)");
+ylabel("y (+:rostal)");
+zlabel("z (+:dorsal)");
 
 figure(3);
 error3d(aves(1,:),aves(2,:),aves(3,:),vars(1,:),vars(2,:),vars(3,:));
+xlabel("x (+:right)");
+ylabel("y (+:rostal)");
+zlabel("z (+:dorsal)");
